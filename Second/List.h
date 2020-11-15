078ff3494ef4fe6fbe8abf1aa015c91a60fc641a
damibran <olegevdk@gmail.com>
2020-11-15 16:18:37 +0300

    List+Array

--- Second/List.h
+++ Second/List.h
@@ -1,10 +1,10 @@
 #pragma once
 #include<string>
-
+#include<iostream>
 
 
 class Set {
-private: 
+private:
 
 	struct Node {
 		char el;
@@ -13,24 +13,260 @@ private:
 		~Node() { delete next; }
 	};
 
-	Node *first=nullptr;
-	Node *last=nullptr;
+	char S;
 
-public: 
+	Node* first = nullptr;
+	Node* last = nullptr;
+
+public:
+	static int N, cnt;
+
 	Set& operator |= (const Set&);
-	Set operator | (const Set&)const; 
+	Set operator | (const Set&)const;
 	Set& operator &= (const Set& b);
 	Set operator & (const Set&)const;
 	Set& operator -= (const Set&);
 	Set operator - (const Set&)const;
-	Set& operator= (const Set& );
-	char* to_String(); 
+	Set& operator= (const Set&);
+	char* to_String();
 	Set() {};
 	Set(const Set&);
 	Set(std::string in);
 	~Set() { delete first; }
+	Set(char S);
 	void add(char c);
 	int get_len();
 	Node* delete_elem(Node* lst);
+	void Show();
+
 };
+
+char* Set::to_String()
+{
+	Node* cur;
+
+	char* res = new char[27];
+	int l = 0;
+	char c;
+
+	for (int i = 0; i < 26; i++)
+	{
+		cur = this->first;
+
+		c = 'a' + i;
+		while (cur != nullptr)
+		{
+			//std::cout << cur->el << '\n';
+			if (cur->el == c)
+				res[l++] = cur->el;
+
+			cur = cur->next;
+		}
+	}
+	res[l] = '\0';
+	return res;
+}
+
+void Set::Show()
+{
+	char* res;
+	res = to_String();
+	std::cout << std::endl << S << " = [" << res << "]" << std::endl;
+	delete res;
+}
+
+Set& Set::operator &= (const Set& B)
+{
+	bool flag = false;
+	Set::Node* curB;
+	Set::Node* curT = this->first;
+
+	while (curT != nullptr)
+	{
+		curB = B.first;
+		flag = false;
+		while (curB != nullptr)
+		{
+			if (curB->el == curT->el)
+				flag = true;
+			curB = curB->next;
+		}
+		if (!flag)
+			curT = this->delete_elem(curT);
+		if (curT != nullptr)
+			curT = curT->next;
+	}
+
+	return (*this);
+}
+
+Set Set::operator & (const Set& B) const
+{
+	Set C(*this);
+	return (C &= B);
+}
+
+Set& Set::operator-= (const Set& B)
+{
+	Node* curT = this->first;
+	Node* curB;
+
+	while (curT != nullptr)
+	{
+		curB = B.first;
+		while (curB != nullptr)
+		{
+			if (curT != nullptr && (curT->el == curB->el))
+			{
+				curT = this->delete_elem(curT);
+			}
+			curB = curB->next;
+		}
+		if (curT != nullptr)
+			curT = curT->next;
+	}
+
+	return (*this);
+}
+
+Set Set::operator - (const Set& B) const
+{
+	Set C(*this);
+	return (C -= B);
+}
+
+Set& Set::operator |= (const Set& B)
+{
+	bool flag = false;
+	Set::Node* curB = B.first;
+	Set::Node* curT;
+
+	while (curB != nullptr)
+	{
+		curT = this->first;
+		flag = false;
+		while (curT != nullptr)
+		{
+			if (curT->el == curB->el)
+				flag = true;
+
+			curT = curT->next;
+		}
+		if (!flag)
+			this->add(curB->el);
+		curB = curB->next;
+	}
+	return (*this);
+}
+
+Set Set::operator | (const Set& B) const
+{
+	Set C(*this);
+	return (C |= B);
+}
+
+Set& Set::operator= (const Set& B)
+{
+	Node* curB = B.first;
+
+	if (this != &B)
+	{
+		if (first != nullptr)
+		{
+			delete (this->first);
+			first = nullptr;
+			last = nullptr;
+		}
+		S = B.S;
+		while (curB != nullptr)
+		{
+			this->add(curB->el);
+			curB = curB->next;
+		}
+	}
+
+	return *this;
+}
+
+Set::Node* Set::delete_elem(Node* lst)
+{
+	Node* temp = nullptr;
+	if (this->get_len() != 0) {
+		if (lst == this->first) {
+			this->first = lst->next;
+			temp = this->first;
+		}
+		else
+		{
+			temp = this->first;
+			while (temp->next != lst)
+			{
+				temp = temp->next;
+			}
+			temp->next = lst->next;
+		}
+		free(lst);
+	}
+	return(temp);
+}
+
+int Set::get_len()
+{
+	Node* cur;
+	int c = 0;
+	cur = this->first;
+	while (cur->next != nullptr)
+	{
+		cur = cur->next;
+		c++;
+	}
+
+	return c;
+}
+
+Set::Set(const Set& a) {
+
+	Node* curA = a.first;
+	S = a.S+1;
+	while (curA != nullptr)
+	{
+		add(curA->el);
+		curA = curA->next;
+	}
+}
+
+Set::Set(char S) : S(S)
+{
+	for (int i = 0; i < N; i++)
+	{
+		if (rand() % 2) add('a' + i);
+	}
+
+	char* res = to_String();
+
+	std::cout << '\n' << S << " = [" << res << "]";
+
+	delete[] res;
+}
+
+Set::Set(std::string in) {
+
+	for (int i = 0; i < in.length(); i++)
+		add(in[i]);
+}
+
+void Set::add(char c) {
+	if (this->first != nullptr) {
+		Node* _new = new Node(c);
+		if (last == nullptr)
+			last = first;
+		last->next = _new;
+		last = _new;
+	}
+	else {
+		Node* _first = new Node(c);
+		first = _first;
+		first->next = nullptr;
+	}
+}
 
--- Second/Main.cpp
+++ Second/Main.cpp
@@ -1,82 +1,27 @@
-#include"List.h"
-#include<iostream>
+#include "List.h"
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
 #include <time.h>
-
-
-char* generate_union() {
-	int m = rand() % 26;
-	char* S = new char[26];
-	char* St = new char[26];
-	for (int i = 0; i < m; i++)
-		S[i] = 49 + rand() % 0x1A + '0';
-	int l = 0;
-	for (int i = 0; i < m; i++) {
-		bool flag = true;
-		for (int j = 0; j < m; j++)
-			if (i - j != 0 && S[i] == S[j]) flag = false;
-		if (flag) {
-			St[l++] = S[i];
-		}
-	}
-	St[l] = '\0';
-	delete[] S;
-	return St;
-}
-
 using namespace std;
+
+int Set::N = 26, Set::cnt = 0;
+const long q0 = 100000;
+
 
 int main()
 {
-	clock_t start, stop;
-	// A&&B\\C||D
-	srand(time(0));
+    srand(time(nullptr));
+    Set A('A'), B('B'), C('C'), D('D'), E;
 
-	const int N = 1000000;
-	bool debug = false;
+    clock_t begin = clock();
+    for (long q = 0; q < q0; q++) E = ((A & B) - C) | D;
+    clock_t end = clock();
 
-	char*** in = new char** [N];
+    cout << endl << "((A & B) \\ C) | D";
+    E.Show();
+    cout << "Time of one = " << (float)(end - begin) / CLK_TCK / q0 << endl;
 
-	for (int j = 0; j < N; j++) {
-		in[j] = new char* [4];
-		for (int i = 0; i < 4; i++)
-		{
-			in[j][i] = generate_union();
-		}
-
-	}
-
-	cout << "FUCK";
-
-	start = clock();
-	for (int i = 0; i < N; i++)
-	{
-		Set a(in[i][0]);
-		if (debug)
-			cout << endl << "a : " << in[i][0] << endl;
-		Set b(in[i][1]);
-		if (debug)
-			cout << "b : " << in[i][1] << endl;
-		Set c(in[i][2]);
-		if (debug)
-			cout << "c : " << in[i][2] << endl;
-		Set d(in[i][3]);
-		if (debug)
-			cout << "d : " << in[i][3] << endl;
-		Set e;
-
-		e = ((a & b) - c) | d;
-
-		if (debug) {
-			char* res;
-			res = e.to_String();
-			cout << "res : " << in[i][3] << endl;
-			delete res;
-		}
-
-	}
-	stop = clock();
-	cout << "\nTime : " << ((float)(stop - start) / CLK_TCK) / N << endl;
-
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
