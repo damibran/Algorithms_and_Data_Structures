dfc3800310909898f45d9cf538eea405a9ca0074
damibran <olegevdk@gmail.com>
2021-03-11 18:41:15 +0300

    remove point

--- Second/screen.h
+++ Second/screen.h
@@ -3,12 +3,6 @@
 #include "Exceptions/CantPutPoint.h"
 #include<iostream>
 
-class point {	//Точка на экране
-public:
-	int x, y;
-	point(int a = 0, int b = 0) : x(a), y(b) {  }
-};
-
 class Screen
 {
 public:
@@ -59,35 +53,7 @@ public:
 	{
 		if (on_screen(a, b)) screen[(YMAX-b) * XMAX + a] = black;
 	}
-	void put_point(point p)
-	{
-		put_point(p.x, p.y);
-	}
-	void put_line(int x0, int y0, int x1, int y1)
-		/* Алгоритм Брезенхэма для прямой:
-		рисование отрезка прямой от (x0,y0) до (x1,y1).
-		Уравнение прямой: b(x-x0) + a(y-y0) = 0.
-		Минимизируется величина abs(eps), где eps = 2*(b(x-x0)) + a(y-y0).  */
-	{
-		int dx = 1;
-		int a = x1 - x0;   if (a < 0) dx = -1, a = -a;
-		int dy = 1;
-		int b = y1 - y0;   if (b < 0) dy = -1, b = -b;
-		int two_a = 2 * a;
-		int two_b = 2 * b;
-		int xcrit = -b + two_a;
-		int eps = 0;
-		for (;;) { //Формирование прямой линии по точкам
-			put_point(x0, y0);
-			if (x0 == x1 && y0 == y1) break;
-			if (eps <= xcrit) x0 += dx, eps += two_b;
-			if (eps >= a || a < b) y0 += dy, eps -= two_a;
-		}
-	}
-	void put_line(point a, point b)
-	{
-		put_line(a.x, a.y, b.x, b.y);
-	}
+
 private:
 	HANDLE hConsle;
 	DWORD dwBytesWritten;
--- Second/shape.h
+++ Second/shape.h
@@ -24,7 +24,7 @@ public:
 class dot: public primitive
 {
 public:
-	dot(point _p):p(glm::vec3(_p.x,_p.y,1)) {}
+	dot(glm::vec2 _p):p(glm::vec3(_p.x,_p.y,1)) {}
 	virtual void drawPrimitive(glm::mat3 trans)override
 	{
 		int x = (trans * p)[0];
@@ -38,7 +38,7 @@ private:
 class edge : public  primitive
 {
 public:
-	edge(point _p1, point _p2) : p1(glm::vec3(_p1.x, _p1.y, 1)), p2(glm::vec3(_p2.x, _p2.y, 1)) {}
+	edge(glm::vec2 _p1, glm::vec2 _p2) : p1(glm::vec3(_p1.x, _p1.y, 1)), p2(glm::vec3(_p2.x, _p2.y, 1)) {}
 	virtual void drawPrimitive(glm::mat3 trans)
 	{
 		int x0 = (trans * p1)[0];
@@ -48,7 +48,28 @@ public:
 		int x1 = (trans * p2)[0];
 		int y1 = (trans * p2)[1];
 
-		gScreen.put_line(x0, y0, x1, y1);
+		put_line(x0, y0, x1, y1);
+	}
+	void put_line(int x0, int y0, int x1, int y1)
+		/* Алгоритм Брезенхэма для прямой:
+		рисование отрезка прямой от (x0,y0) до (x1,y1).
+		Уравнение прямой: b(x-x0) + a(y-y0) = 0.
+		Минимизируется величина abs(eps), где eps = 2*(b(x-x0)) + a(y-y0).  */
+	{
+		int dx = 1;
+		int a = x1 - x0;   if (a < 0) dx = -1, a = -a;
+		int dy = 1;
+		int b = y1 - y0;   if (b < 0) dy = -1, b = -b;
+		int two_a = 2 * a;
+		int two_b = 2 * b;
+		int xcrit = -b + two_a;
+		int eps = 0;
+		for (;;) { //Формирование прямой линии по точкам
+			gScreen.put_point(x0, y0);
+			if (x0 == x1 && y0 == y1) break;
+			if (eps <= xcrit) x0 += dx, eps += two_b;
+			if (eps >= a || a < b) y0 += dy, eps -= two_a;
+		}
 	}
 private:
 	glm::vec3 p1, p2;
